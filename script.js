901af015b0571e4eb2bb2fa80a1e833eaa2294c7
AnanyaTiwari850 <ananyatiwari850@gmail.com>
2025-12-25 16:05:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.innerHTML = `
+        ${taskText}
+        <button onclick="removeTask(this)">X</button>
+    `;
+
+    li.onclick = function () {
+        li.classList.toggle("completed");
+    };
+
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
+
+function removeTask(btn) {
+    btn.parentElement.remove();
+} 
